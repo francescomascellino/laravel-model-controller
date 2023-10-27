d0e4f6c08af73674f3faaa53479742311d175bfb
francescomascellino <franesco.mascellino@gmail.com>
2023-10-27 15:37:37 +0200

    Add cards

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,16 +4,29 @@
 @section('title', 'Movies')
 
 @section('content')
-    <h1>MOVIES</h1>
+
     <main>
         <div class="container">
-            <div class="row row-cols-1 row-cols-md-5">
+            <h1 class="text-center mt-3">MOVIES</h1>
+            <div class="row row-cols-1 row-cols-md-2 g-4 my-3">
 
                 @forelse ($movies as $movie)
                     <div class="col">
-                        {{ $movie->title }}
+
+                        <div class="card shadow h-100">
+                            <div class="card-body">
+                                <h4 class="card-title">{{ $movie->title }}</h4>
+                                <p><strong>Original title: </strong>{{ $movie->original_title }}</p>
+                                <p><strong>Nationality: </strong>{{ $movie->nationality }}</p>
+                                <p><strong>Released on: </strong>{{ $movie->date }}</p>
+                                <p><strong>Vote on: </strong>{{ $movie->vote }}</p>
+                            </div>
+                        </div>
+
                     </div>
+
                 @empty
+
                     <div class="col">
                         <h2>No Movies in the database! 😥</h2>
                     </div>
