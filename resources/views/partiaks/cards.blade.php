6046d19d0ed90f3da793dd9cd2fda5b4704bbff4
francescomascellino <franesco.mascellino@gmail.com>
2023-10-27 16:11:10 +0200

    Add random images for cards using $movie->id

--- resources/views/partiaks/cards.blade.php
+++ resources/views/partiaks/cards.blade.php
@@ -3,6 +3,10 @@
 
         <div class="card shadow h-100">
             <div class="card-body">
+
+                <img src="https://picsum.photos/400/200?random={{ $movie->id }}" class="card-img-top rounded-2 mb-3"
+                    alt="{{ $movie->original_title }}">
+
                 <h4 class="card-title">{{ $movie->title }}</h4>
                 <p><strong>Original title: </strong>{{ $movie->original_title }}</p>
                 <p><strong>Nationality: </strong>{{ $movie->nationality }}</p>
